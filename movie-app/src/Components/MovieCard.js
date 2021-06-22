7bc574c92f6731132443a4d7f25829d029b27aed
Connie Lai <clai011@ucr.edu>
2021-06-22 15:14:39 -0700

    Removed same function definition and renamed event handler.

--- movie-app/src/Components/MovieCard.js
+++ movie-app/src/Components/MovieCard.js
@@ -7,25 +7,18 @@ const MovieCard = ({movie}) => {
   }
 
   const [cardHover, setCardHover] = useState(false);
-  const showInfoOnHover = () => {
-    setCardHover(!cardHover)
-  }
-
-  const hideInfoOnHover = () => {
+  const toggleInfoHover = () => {
     setCardHover(!cardHover)
   }
 
   return (
-    <div className="card" style={moviePoster} onMouseEnter ={showInfoOnHover} onMouseLeave={hideInfoOnHover}>
-
+    <div className="card" style={moviePoster} onMouseEnter ={toggleInfoHover} onMouseLeave={toggleInfoHover}>
       <div className={`info-container ${cardHover? `hide-info`: `show-info`}`}>
         <h2>{movie.Title}</h2>
         <h3>{movie.Year}</h3>
         <button className="detail-button">Details</button>
       </div>
-
       <div className={`title-overlay ${cardHover ? `overlay-hide`: `overlay-show`}`}></div>
-
     </div>
   );
 };
