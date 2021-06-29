4dba89c4715adff20dd770e99e4f5e24e39d68f0
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-06-29 11:01:36 -0700

    feat(search-pagination):adds pagination

--- movie-app/src/body/Searched.js
+++ movie-app/src/body/Searched.js
@@ -1,20 +1,38 @@
-import MovieCard from '../components/MovieCard'
+import MovieCard from "../components/MovieCard";
 
-const Searched = ({ movies }) => {
-    const moviesCheck = () => {
-        if (movies){
-            return movies.map(movie => {
-                return <MovieCard key={movie.imdbID} movie={movie}/>
-            })
-        } else {
-            return <div>Loading...</div>
-        }
+const Searched = ({ totalResults, movies, setPage, page }) => {
+  const moviesCheck = () => {
+    if (movies) {
+      return movies.map((movie) => {
+        return <MovieCard key={movie.imdbID} movie={movie} />;
+      });
+    } else {
+      return <div>Loading...</div>;
     }
-    return(
-        <div className="search-container">
-            {moviesCheck()}
-       </div>
-    )
-}
+  };
+
+  let movieRemainder = parseInt(totalResults)%10 === 0 ? parseInt(totalResults)/10 : Math.floor(parseInt(totalResults)/10 + 1)
+
+  return (
+    <>
+      <div className="search-container">{moviesCheck()}</div>
+      <div className="page-btn-container">
+        <button
+            className={page > 1 ? "back-button" : "hide-back-button"}
+            onClick={() => setPage(page - 1)}
+        >
+            <img src="next.png" alt="" className="back-page-btn" />
+        </button>
+        <div className="page-number">{page}/{movieRemainder}</div>
+        <button
+            className={page < movieRemainder ? "back-button" : "hide-back-button"}
+            onClick={() => setPage(page + 1)}
+        >
+            <img src="next.png" alt="" className="next-page-btn"/>
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Searched;
